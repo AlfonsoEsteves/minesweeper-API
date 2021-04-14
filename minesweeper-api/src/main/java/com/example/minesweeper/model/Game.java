ca4b79799dd892020713ab653e232fefeeb139c8
alfonsoesteves93 <alfonsoesteves93@gmail.com>
2021-04-14 01:11:27 -0300

    Adding the minesweeper game logic

---
+++ minesweeper-api/src/main/java/com/example/minesweeper/model/Game.java
@@ -0,0 +1,102 @@
+package com.example.minesweeper.model;
+
+import java.util.Random;
+
+public class Game {
+
+    public static final char MINE = '*';
+    public static final char COVERED_CELL = '.';
+    public static final char UNCOVERED_CELL = ' ';
+    public static final char FLAG_MARK = 'x';
+    public static final char QUESTION_MARK = '?';
+
+    public int rows;
+    public int columns;
+    public int[][] cellMines; // numbers from 0 to 8 or a '*' sign that represents a mine
+    public char[][] cellState; // ' ' for uncovered cells, '.' for hidden cells, 'x' for mine marks, '?' for question marks
+
+    public Game(int rows, int columns, int mines) {
+        this.rows = rows;
+        this.columns = columns;
+
+        if(rows * columns > mines) {
+            throw new RuntimeException("Invalid amount of mines");
+        }
+
+        for(int x = 0; x < rows; x++) {
+            for(int y = 0; y < columns; y++) {
+                cellMines[x][y] = '0';
+                cellState[x][y] = COVERED_CELL;
+            }
+        }
+        Random random = new Random();
+        for(int placedMines = 0; placedMines < mines; placedMines++) {
+            int x = random.nextInt(rows);
+            int y = random.nextInt(columns);
+            while(cellState[x][y] == '*') {
+                x++;
+                if(x == rows) {
+                    x = 0;
+                    y = (y + 1) % columns;
+                }
+            }
+            cellMines[x][y] = '*';
+            for (int adjacentX = x - 1; adjacentX <= x + 1; adjacentX++) {
+                for (int adjacentY = y - 1; adjacentY <= y + 1; adjacentY++) {
+                    if (adjacentX >= 0 && adjacentX < rows && adjacentY >= 0 && adjacentY < columns) {
+                        if (cellMines[adjacentX][adjacentY] != MINE) {
+                            cellMines[adjacentX][adjacentY]++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Uncovers a the cell in the position (x, y)
+     *
+     * @param x row
+     * @param y column
+     * @return true if it was a valid action, false if the player uncovered a mine
+     */
+    public boolean uncoverCell(int x, int y) {
+        cellState[x][y] = UNCOVERED_CELL;
+        if(cellMines[x][y] == MINE) {
+            for(int i = 0; i < rows; i++) {
+                for(int j = 0; j < columns; j++) {
+                    cellState[x][y] = UNCOVERED_CELL;
+                }
+            }
+            return false;
+        }
+        else {
+            if(cellMines[x][y] == 0) {
+                for (int adjacentX = x - 1; adjacentX <= x + 1; adjacentX++) {
+                    for (int adjacentY = y - 1; adjacentY <= y + 1; adjacentY++) {
+                        if (adjacentX >= 0 && adjacentX < rows && adjacentY >= 0 && adjacentY < columns) {
+                            if (cellState[adjacentX][adjacentY] != UNCOVERED_CELL) {
+                                if(!uncoverCell(x, y)) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void addFlagMark(int x, int y) {
+        if (cellState[x][y] != UNCOVERED_CELL) {
+            cellState[x][y] = FLAG_MARK;
+        }
+    }
+
+    public void addQuestionMark(int x, int y) {
+        if (cellState[x][y] != UNCOVERED_CELL) {
+            cellState[x][y] = QUESTION_MARK;
+        }
+    }
+}
